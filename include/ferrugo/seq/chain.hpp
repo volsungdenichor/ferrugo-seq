38ac2fbd6b203e22d672dacdc662e9b974e48885
krzysiek <krzysztof.muhleisen@gmail.com>
2024-10-03 17:55:30 +0200

    chain more

--- include/ferrugo/seq/chain.hpp
+++ include/ferrugo/seq/chain.hpp
@@ -38,6 +38,25 @@ struct chain_fn
         }
     };
 
+    template <class T0, class T1, class T2, class T3, class Out = std::common_type_t<T0, T1, T2, T3>>
+    auto operator()(const sequence<T0>& s0, const sequence<T1>& s1, const sequence<T2>& s2, const sequence<T3>& s3) const
+        -> sequence<Out>
+    {
+        return (*this)((*this)((*this)(s0, s1), s2), s3);
+    }
+
+    template <class T0, class T1, class T2, class Out = std::common_type_t<T0, T1, T2>>
+    auto operator()(const sequence<T0>& s0, const sequence<T1>& s1, const sequence<T2>& s2) const -> sequence<Out>
+    {
+        return (*this)((*this)(s0, s1), s2);
+    }
+
+    template <class T0, class T1, class Out = std::common_type_t<T0, T1>>
+    auto operator()(const sequence<T0>& s0, const sequence<T1>& s1) const -> sequence<Out>
+    {
+        return (*this)(sequence<Out>{ s0 }, sequence<Out>{ s1 });
+    }
+
     template <class T>
     auto operator()(const sequence<T>& lhs, const sequence<T>& rhs) const -> sequence<T>
     {
